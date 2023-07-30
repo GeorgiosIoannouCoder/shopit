8b7961da8ab59434c6062181fdff5ef527f4fed4
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2023-07-30 08:06:35 -0400

    Create login form screen UI, user login header UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,13 +9,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./store";
 // import "./assets/styles/bootstrap.original.css";
 import "./assets/styles/bootstrap.new.css";
 import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-import store from "./store";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
